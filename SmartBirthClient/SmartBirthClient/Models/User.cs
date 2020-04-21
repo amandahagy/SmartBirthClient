9e7bb6a0ddc69f6533638659b80cad4ea935fc79
Amanda Hagy <amandahagy@hotmail.com>
2020-04-21 18:51:46 -0300

    User model

---
+++ SmartBirthClient/SmartBirthClient/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SmartBirthClient.Models
+{
+    public class User
+    {
+        public int UserCode { get; set; }
+        public string Name { get; set; }
+        public long Rg { get; set; }
+        public long Cpf { get; set; }
+        public DateTime Birth { get; set; }
+
+        public List<BirthRegistration> Heirs { get; set; }
+
+        public User()
+        {
+            this.Heirs = new List<BirthRegistration>();
+        }
+    }
+}
