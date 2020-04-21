2aa5ca3f64c853fbf80c037f4ac6f9d11c6da0e8
Amanda Hagy <amandahagy@hotmail.com>
2020-04-21 18:46:40 -0300

    Json post request

---
+++ SmartBirthClient/SmartBirthClient/GetJson.cs
@@ -0,0 +1,27 @@
+﻿/*using System;
+
+namespace SmartBirthClient
+{
+    class GetJson
+    {
+        static void Main(string[] args)
+        {
+            get();
+            Console.Read();
+        }
+
+        static void get()
+        {
+            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+            System.Net.Http.HttpResponseMessage response =
+                client.GetAsync("http://127.0.0.1:8080/api/User").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                Console.Write(content.ToString());
+            }
+        }
+    }
+}*/
--- SmartBirthClient/SmartBirthClient/Program.cs
+++ SmartBirthClient/SmartBirthClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text;
 
 namespace SmartBirthClient
 {
@@ -6,21 +8,25 @@ namespace SmartBirthClient
     {
         static void Main(string[] args)
         {
-            get();
+            post();
             Console.Read();
         }
 
-        static void get()
+        static void post()
         {
             System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+
+            String json = "{ 'UserCode': 100,    'Name': 'Maria Magalhaes',    'Rg': 12345678901,   'Cpf': 12345678977,   'Birth': '2012-03-19T07:22Z' }";
+
+            StringContent content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+
             System.Net.Http.HttpResponseMessage response =
-                client.GetAsync("http://127.0.0.1:8080/api/User").Result;
+                client.PostAsync("http://127.0.0.1:8080/api/User", content).Result;
 
             if (response.IsSuccessStatusCode)
             {
-                string content = response.Content.ReadAsStringAsync().Result;
-
-                Console.Write(content.ToString());
+                Console.WriteLine("The user has been successfully created.");
+                Console.Write("Link: " + response.Headers.Location);
             }
         }
     }
