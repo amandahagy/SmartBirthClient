51adec38922bc99e6bf951bece82055d2adacf5a
Amanda Hagy <amandahagy@hotmail.com>
2020-04-21 18:57:22 -0300

    Reference on user class and Birth Registration Model

---
+++ SmartBirthClient/SmartBirthClient/Models/BirthRegistration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmartBirthClient.Models
+{
+    public class BirthRegistration
+    {
+        public int BirthRegistrationCode { get; set; }
+        public string NewbornName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public int Parent1Code { get; set; }
+        public int Parent2Code { get; set; }
+    }
+}
